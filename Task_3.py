a394386e36d0abc01853b00b68c91b74c98ca6a0
Aleksandr <fomichov73@mail.ru>
2022-12-04 13:27:32 +0300

    Added solution Task 3

--- Task_3.py
+++ Task_3.py
@@ -1,3 +1,9 @@
+#3-Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу 
+# между максимальным и минимальным значением дробной части элементов.
+#Пример:
+#[1.1, 1.2, 3.1, 5.17, 10.02] => 0.18 или 18
+#[4.07, 5.1, 8.2444, 6.98] - 0.91 или 91
+import random 
 
 def give_int(input_string):
     while True:
@@ -7,12 +13,20 @@ def give_int(input_string):
         except:
             print('Попробуйте еще раз. Вы ввели не число')
 
-namber = give_int("Введите число N: ")
-numbers_list = []
-for i in range(1, namber+1):
-    if i == 1:
-        numbers_list.append(i)
-    else:
-        new_element = numbers_list[i-2]*i
-        numbers_list.append(new_element)
-print (f"Набор произведений чисел от 1 до {namber}: {numbers_list}")
+namber = give_int("Введите число (колличество элементов в массиве): ")
+max_namber_list = give_int("Введите число (максимально возможный элемент массива): ")
+rand_list = []
+new_list = []
+max_part = 0
+min_part = 1
+for i in range(0,namber):
+    rand_list.append(round(random.uniform(0,max_namber_list),4))
+    fractional_num = round((rand_list[i] % 1),4)  
+    new_list.append(fractional_num)
+    if max_part < new_list[i]:
+        max_part = new_list[i]
+    if min_part > new_list[i]:
+        min_part = new_list[i]
+    result = round((max_part - min_part),4)   
+           
+print(f'Разницу между максимальным {max_part} и минимальным {min_part} значением \nдробной части элементов массива \n{rand_list} равна: {result}') 
--- Task_4.py
+++ Task_4.py
@@ -1,5 +1,9 @@
-#N = int(input("Введите размер списка: "))
-import random
+#4- Напишите программу, которая будет преобразовывать десятичное число в двоичное. 
+# Подумайте, как это можно решить с помощью рекурсии.Не использовать функцию bin
+# Пример:
+# 45 -> 101101
+# 3 -> 11
+# 2 -> 10
 def give_int(input_string):
     while True:
         try:
@@ -7,6 +11,22 @@ def give_int(input_string):
             return num
         except:
             print('Попробуйте еще раз. Вы ввели не число')
-k = give_int("Введите размер списка: ")           
-spam = list(range(1, k+1))
-print(spam)
+a = give_int('Введите целое число: ')  
+b = give_int('Введите целое число: ') 
+list = []
+result = 0         
+def converting_num(a, b):
+    ost = a % b
+     
+    if ost !=0:
+        result = a // b
+        list.insert(0,ost)
+    return result
+    #     if number % sistema == 1:
+    #         list.append(1)
+    #     return    
+    # number = converting_num
+    # return list 
+    
+print(list)
+print(result)
